e56993ffb7eed24bc40a99ce09a9981829d20edc
Fábio França <fabiotibete@gmail.com>
2020-08-02 17:30:56 -0300

    project setup and DataModel

---
+++ GoodEatins/GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by Fábio França on 02/08/20.
+//  Copyright © 2020 music.com.chama. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
---
+++ GoodEatins/GoodEatins/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by Fábio França on 02/08/20.
+//  Copyright © 2020 music.com.chama. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
