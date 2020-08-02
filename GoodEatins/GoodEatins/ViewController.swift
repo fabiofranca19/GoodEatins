5526efbeb0fe0b0c98136007b36472182fb822fc
Fábio França <fabiotibete@gmail.com>
2020-08-02 17:19:47 -0300

    Create Project

---
+++ GoodEatins/GoodEatins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins
+//
+//  Created by Fábio França on 02/08/20.
+//  Copyright © 2020 music.com.chama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
